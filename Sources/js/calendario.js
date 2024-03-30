23f6a97b0a69bdf2be74a25cb0a46a4310289801
Adrian <aovidiucocean02@gmail.com>
2024-03-30 10:57:32 +0100

    Eventos calendario JSON hecho, falta estilo

--- Sources/js/calendario.js
+++ Sources/js/calendario.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
+    var events;
+    $.getJSON("../json/calendario/eventos.json", function(data) {
+      events = data;
+    });
+
     $('li').click(function() {
       var day = $(this).text();
-      var eventDetails = "Details for Day " + day; // Example event details
-      $('#event-details header h2').text(eventDetails);
-      $('#event-details').show();
+      var eventFound = false;
+
+      // Check if there is an event on the clicked day
+      for (var i = 0; i < events.length; i++) {
+        if (events[i].day == day) {
+          var eventHeader = events[i].name;
+          $('#event-details header h2').text(eventHeader);
+          $('#event-details .content').text(events[i].description);
+          $('#event-details').show();
+          eventFound = true;
+          break;
+        }
+      }
+      // If no event found, hide event details box
+      if (!eventFound) {
+        $('#event-details').hide();
+      }
     });
   
     $('#close').click(function() {
